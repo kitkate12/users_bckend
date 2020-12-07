a8ead64b59f7d9c73b3efd6dacceee33131a223f
Caterina Natali <caterinanatali12@gmail.com>
2020-12-07 11:50:40 +0100

    add patch

--- routes/index.js
+++ routes/index.js
@@ -13,52 +13,75 @@ const db = admin.firestore();
 
 //id univooci generator
 function genId(users) {
-    return users.length > 0  ? Math.max(...users.map(user => user.id)) + 1 : 1
+    return users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 1
 }
 
 //GET LIST
 router.get("/users", async (req, res) => {
+    //aggiorno la lista
+    users.length = 0;
+    
     const list = await db.collection('users').get();
-    list.forEach(doc => users.push(doc.data())); 
+    list.forEach(doc => users.push(doc.data()));
     return res.json(users);
 });
 
 //GET ONE BY ID
 router.get("/users/:id", (req, res) => {
     db.collection('users').doc(req.params.id).get()
-    .then(
-        user => {
-            if(!user.exists){
-                res.status(404).json({message:"User not found"});
+        .then(
+            user => {
+                if (!user.exists) {
+                    res.status(404).json({ message: "User not found" });
+                }
+                return res.status(200).json(user.data());
             }
-            return res.status(200).json(user.data());
-        } 
-    ).catch(error => res.status(500).send(error));   
+        ).catch(error => res.status(500).send(error));
 });
 
 //POST 
 router.post("/users", async (req, res) => {
     const list = await db.collection('users').get();
-    list.forEach(doc => users.push(doc.data())); 
+    list.forEach(doc => users.push(doc.data()));
     const newId = genId(users);
     let user = {
         id: newId,
         name: req.body.name
     };
     users.push(user);
-    db.collection('users').add(user);
-    return res.status(201).json({ message: "Created"});
+    db.collection('users').doc(newId.toString()).set(user);
+    return res.status(201).json({ message: "Created" });
 });
-router.patch("/users/:id", (req, res) => {
+
+//PATCH
+router.patch("/users/:id", async (req, res) => {
+    //aggiorno la lista
+    users.length = 0;
+    const list = await db.collection('users').get();
+    list.forEach(doc => users.push(doc.data()));
+
+    if (!req.body.name) {
+        return res.status(400).json({ message: "You have to pass a name" });
+    }
+
+    const u = await db.collection('users').doc(req.params.id).get();
+
+    if (!u.data()) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    db.collection('users').doc(req.params.id).set({ name: req.body.name }, { merge: true });
+
     const user = users.find(val => val.id === Number(req.params.id));
     // errore
-    user.name = req.body.name;
-    return res.json({massage: "Updated"});
+    //user.name = req.body.name;
+    return res.json({ massage: "Updated" });
 });
+
+//DELETE
 router.delete("/users/:id", (req, res) => {
-    //errore
     const userIndex = users.findIndex(val => val.id === Number(req.params.id));
     users.splice(userIndex, 1);
-    return res.json({message: "Deleted"});
+    return res.json({ message: "Deleted" });
 })
 module.exports = router; 
